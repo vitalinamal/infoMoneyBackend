f13a59fbb622e4b13788e4d45205f2c2678bb2ba
SandwichDream <karnll.adnitorll@gmail.com>
2023-12-05 16:47:57 +0300

    changes to yml and specifiy query named parameter

--- src/main/java/prog/academy/infomoney/repository/TokenRepository.java
+++ src/main/java/prog/academy/infomoney/repository/TokenRepository.java
@@ -2,6 +2,7 @@ package prog.academy.infomoney.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import prog.academy.infomoney.entity.Token;
 
@@ -14,9 +15,9 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
     @Query(value = """
             select t from Token t join fetch User u
             on t.user.id = u.id
-            where u.id = :id and (t.expired = false or t.revoked = false)
+            where u.id = :tokenId and (t.expired = false or t.revoked = false)
             """)
-    List<Token> findAllValidTokenByUser(Long id);
+    List<Token> findAllValidTokenByUser(@Param("tokenId") Long id);
 
     Optional<Token> findByJwtToken(String token);
 }
